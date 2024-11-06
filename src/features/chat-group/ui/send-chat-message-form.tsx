c3e1e8a10bceeed197b6a3254a64c6a7fe65862a
balanced <vladyslav.herasymenko@infotech.gov.ua>
2024-11-06 23:02:30 +0200

    fix: empty message sent on chat

--- src/features/chat-group/ui/send-chat-message-form.tsx
+++ src/features/chat-group/ui/send-chat-message-form.tsx
@@ -51,7 +51,7 @@ export const SendChatMessageForm = () =>
             disabled={disabled}
             aria-label='Send Message'
             data-testid='send-button'
-            className='mb-1 ml-2 flex h-8 w-8 items-center justify-center rounded-full bg-black text-white transition-colors hover:opacity-70 focus-visible:outline-none focus-visible:outline-black disabled:text-[#f4f4f4] disabled:hover:opacity-100 dark:bg-white dark:text-black dark:focus-visible:outline-white disabled:dark:bg-token-text-quaternary dark:disabled:text-token-main-surface-secondary disabled:bg-[#D7D7D7]'
+            className='ml-2 flex h-8 w-8 items-center justify-center rounded-full bg-black text-white transition-colors hover:opacity-70 focus-visible:outline-none focus-visible:outline-black disabled:text-[#f4f4f4] disabled:hover:opacity-100 dark:bg-white dark:text-black dark:focus-visible:outline-white disabled:dark:bg-token-text-quaternary dark:disabled:text-token-main-surface-secondary disabled:bg-[#D7D7D7]'
           >
             <Icon name='sprite/send-request-icon' fontSize={24} />
           </button>
